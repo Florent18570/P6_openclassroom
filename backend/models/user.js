7da11fc378ff367501dfcd0eccd2e101f738c78e
Florent <florent.escots@gmail.com>
2022-07-23 14:13:52 +0200

    signup

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
 const userschema = mongoose.Schema({
-  email: String,
-  password: String,
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, unique: true },
 });
+
+userschema.plugin(UniqueValidator);
 
 module.exports = mongoose.model("User", userschema);
--- backend/routes/authentification.js
+++ backend/routes/authentification.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const authcontrol = require("../controllers/authentification.js");
 
-router.post("/", authcontrol.signup);
+router.post("/signup", authcontrol.signup);
+
+router.post("/login", authcontrol.login);
 
 module.exports = router;
